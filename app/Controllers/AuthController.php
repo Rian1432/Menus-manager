549fc6e65a0dd83eb60b451cd052ded0ebfdeb06
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-11-27 21:09:27 -0300

    feat: Criação do login de usuário admin

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\User;
+use Core\Http\Controllers\Controller;
+use Core\Http\Request;
+use Lib\Authentication\Auth;
+use Lib\FlashMessage;
+
+class AuthController extends Controller
+{
+    public string $layout = 'login';
+
+    public function index(): void
+    {
+        $title = 'Login';
+        $this->render('auth/login', compact('title'));
+    }
+    
+    public function authenticate(Request $request): void
+    {
+        $params = $request->getParam('user');
+        $user = User::findByEmail($params['email']);
+
+        if ($user && $user->authenticate($params['password'])) {
+            Auth::login($user);
+
+            FlashMessage::success('Login realizado com sucesso!');
+            $this->redirectTo(route('root'));
+        } else {
+            FlashMessage::danger('Email e/ou senha inválidos!');
+            $this->redirectTo(route('users.login'));
+        }
+    }
+
+    public function destroy(): void
+    {
+        Auth::logout();
+        FlashMessage::success('Logout realizado com sucesso!');
+        $this->redirectTo(route('users.login'));
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use Core\Router\Route;
 
 // Authentication
-Route::get('/', [HomeController::class, 'index'])->name('root');
+Route::get('/login', [AuthController::class, 'index'])->name('users.login');
+Route::post('/login', [AuthController::class, 'authenticate']);
+
+// Logout
+Route::get('/logout', [AuthController::class, 'destroy']);
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('root');
+    
+});
