adb1cea62a61e52d2f34a5431cfe17d579840783
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2025-02-17 14:35:37 -0300

    fix: Corrige controller de pedidos

--- app/Controllers/OrderController.php
+++ app/Controllers/OrderController.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 
 use App\Models\ClientTable;
-use App\Models\Order;
 use Core\Http\Controllers\Controller;
 use Core\Http\Request;
 use Lib\FlashMessage;
@@ -15,7 +14,7 @@ class OrderController extends Controller
     public function index(Request $request): void
     {
         $title = 'Pedidos';
-        $table = ClientTable::findById($request->getParam('table_number'));
+        $table = ClientTable::findByTableNumber($request->getParam('table_number'));
         $paginator = $table->orders()->paginate(page: $request->getParam('page', 1), route: route('table.orders.paginate', ['table_number' => $table->table_number]));
         $orders = $paginator->registers();
 
@@ -25,7 +24,7 @@ class OrderController extends Controller
     public function show(Request $request): void
     {
         $title = 'Visualizar pedido';
-        $table = ClientTable::findById($request->getParam('table_number'));
+        $table = ClientTable::findByTableNumber($request->getParam('table_number'));
         $order = $table->orders()->findById($request->getParam('id'));
 
         $this->render('client/orders/show', compact('title', 'order', 'table'));
@@ -33,19 +32,15 @@ class OrderController extends Controller
 
     public function create(Request $request): void
     {
-        $data =  [
-            // 'client_table_id' => intval($request->getParam('table_number')),
-            'client_table_id' => 40,
-            'status' => 'open',
-        ];
-
-        $order = new Order($data);
+        $table = ClientTable::findByTableNumber($request->getParam('table_number'));
+        $order = $table->orders()->new(['status' => 'open']);
 
         if ($order->save()) {
             FlashMessage::success('Pedido criado com sucesso');
             $this->redirectTo(route('table.orders.paginate', ['table_number' => $request->getParam('table_number')]));
         } else {
             FlashMessage::danger('Erro ao criar pedido');
+            $this->redirectTo(route('table.orders.paginate', ['table_number' => $request->getParam('table_number')]));
         }
     }
 }
